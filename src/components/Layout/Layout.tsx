9e13ee497f8d0cd042a6ac595eef50ffceec05f2
Indranil Halder <indranilhalder.dev@gmail.com>
2023-02-03 00:16:09 +0530

    Added SEO

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from "react";
-import Head from "next/head";
+import { Seo } from "../Seo";
 import { Navbar } from "../Navbar";
 import { Footer } from "../Footer";
 
@@ -10,10 +10,7 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children }) => {
     return (
         <>
-            <Head>
-                <title>Indranil Halder | Portfolio</title>
-                <link rel="icon" href="/logo.png" type="image/x-icon" />
-            </Head>
+            <Seo />
             <div className="bg-slate-900 text-white">
                 <Navbar />
                 <main className="container max-w-4xl mx-auto py-8 px-8 md:px-0">
---
+++ src/components/Seo/Seo.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import Head from "next/head";
+
+const seo = {
+    image: "/screenshot.png",
+    description:
+        "Hello I am Indranil Halder and I am a MERN Stack developer JavaScript enthusiast who has a predilection for design and enjoy working on projects where approaching design and development as co-dependent processes can lead to smarter solutions and a better overall product experience for human beings.",
+    title: "Indranil Halder | Portfolio",
+    url: "https://github.com/indranildeveloper",
+    twitterUsername: "@indranilcoder",
+};
+
+const Seo: FC = () => {
+    return (
+        <Head>
+            <html lang="en" />
+
+            <meta name="description" content={seo.description} />
+            <meta name="image" content={seo.image} />
+
+            <meta property="og:title" content={seo.title} />
+            <meta property="og:description" content={seo.description} />
+            <meta property="og:image" content={seo.image} />
+            <meta property="og:url" content={seo.url} />
+            <meta property="og:type" content="website" />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content={seo.twitterUsername} />
+            <meta name="twitter:title" content={seo.title} />
+            <meta name="twitter:description" content={seo.description} />
+            <meta name="twitter:image" content={seo.image} />
+        </Head>
+    );
+};
+
+export default Seo;
